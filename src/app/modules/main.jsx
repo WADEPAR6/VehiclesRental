3aaf1ba54a9f4ee15073332bd2b4baa2b90dc279
WADEPAR6 <kparedes9203@uta.edu.ec>
2024-09-24 21:12:23 -0500

    first commit

---
+++ src/app/modules/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Main() {
+    return
+    <div>
+        <div>
+            MainPage
+        </div>
+        <div>
+            Main
+        </div>
+    </div>;
+
+
+}
+
+export default Main;
